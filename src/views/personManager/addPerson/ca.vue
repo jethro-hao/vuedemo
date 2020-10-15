3b3dd7274ef364c5240c67e75c92775edbf432c0
hao_zhuo <hao_zhuo@axintsoft.com>
2020-10-15 16:02:50 +0800

    抓拍

--- src/views/personManager/addPerson/ca.vue
+++ src/views/personManager/addPerson/ca.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="wrap">
+  <div class="">
     <camera></camera>
   </div>
 </template>
 <script>
-import camera from "./camera.vue";
+// import camera from "./camera.vue";
 // import camera from "./camera2.vue";
 // import camera from "./camera3.vue";
 // import camera from "./camera4.vue";
+import camera from "@/components/photograph/camera.vue";
 export default {
   components: {
     camera,
